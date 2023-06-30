fd5a82af536d63ecfeee68ac637a79acb9a56e15
yasbu9015 <138145776+yasbu9015@users.noreply.github.com>
2023-06-30 14:32:44 +0000

    Edited the twili project

--- moretwi/prepare.py
+++ moretwi/prepare.py
@@ -6,7 +6,7 @@ import numpy as np
 # download the tiny shakespeare dataset
 input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
 if not os.path.exists(input_file_path):
-    data_url = 'https://raw.githubusercontent.com/ilikepegasi/Datasets/main/greekText.txt'
+    data_url = 'https://raw.githubusercontent.com/yasbu9015/Datasets/main/moretwilight.txt'
     with open(input_file_path, 'w') as f:
         f.write(requests.get(data_url).text)
 
